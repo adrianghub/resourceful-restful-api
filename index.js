3690dea619d8f60bb5314ec643b5eb40c2fde607
Adrian <adrian_zinko@op.pl>
2021-05-15 12:29:55 +0200

    Connect app to mongo db

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 import express from 'express';
-import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+
+const app = express();
+
+app.use(express.urlencoded({ limit: "25mb", extended: true }));
+app.use(express.json({ limit: "25mb", extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+const CONNECT_URL = "mongodb+srv://adrianghub:adrianghub001@cluster0.ao8mk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+mongoose.connect(CONNECT_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((err) => console.log(err.message))
+
+mongoose.set('useFindAndModify', false);
