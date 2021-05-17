d09155932e8859851fd542a766ce169184fca50f
Adrian <adrian_zinko@op.pl>
2021-05-17 09:08:24 +0200

    Configure port for heroku

--- index.js
+++ index.js
@@ -18,15 +18,13 @@ app.get('/', (req, res) => {
   res.send('Snippet API');
 })
 
-const PORT = process.env.PORT || 5000;
-
 mongoose
   .connect(process.env.CONNECT_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+    app.listen(process.env.PORT || 5000, () => console.log(`Server running on port: ${PORT}`))
   )
   .catch((err) => console.log(err.message));
 
