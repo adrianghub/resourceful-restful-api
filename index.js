744d69aca8d14aa3652d25acf570f80117bf343c
Adrian <adrian_zinko@op.pl>
2021-05-17 08:44:42 +0200

    configurate env vars

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-import express from 'express';
-import mongoose from 'mongoose'
-import cors from 'cors'
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
-import postRoutes from './routes/posts.js';
+import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(express.urlencoded({ limit: "25mb", extended: true }));
 app.use(express.json({ limit: "25mb", extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes);
+app.use("/posts", postRoutes);
 
 const PORT = process.env.PORT || 5000;
-const CONNECT_URL = "mongodb+srv://adrianghub:adrianghub001@cluster0.ao8mk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-mongoose.connect(CONNECT_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-  .catch((err) => console.log(err.message))
+mongoose
+  .connect(process.env.CONNECT_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((err) => console.log(err.message));
 
-mongoose.set('useFindAndModify', false);
+mongoose.set("useFindAndModify", false);
